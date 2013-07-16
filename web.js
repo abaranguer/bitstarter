984fb6ffe6c6ce822a3e2011a9f6d045cb8f7cb8
Albert Baranguer <abaranguer@gmail.com>
2013-07-17 00:15:43 +0200

    homework 4.part 1

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer(256);
+var htmlfile = "/home/albert/wk-node/bitstarter/index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buf = fs.readFileSync('index.html');
-    response.send(buf.toString());
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
